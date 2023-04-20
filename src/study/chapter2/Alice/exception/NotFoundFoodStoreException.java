24c0fe1a68569e80e941a468983254f2175d5257
eom-tae-in <eti0728@daum.net>
2023-04-21 00:56:56 +0900

    [feat] 입력으로 들어온 음식을 파는 음식가게를 찾지 못했을 때 발생시킬 예외클래스 생성

---
+++ src/study/chapter2/Alice/exception/NotFoundFoodStoreException.java
@@ -0,0 +1,7 @@
+package src.study.chapter2.Alice.exception;
+
+public class NotFoundFoodStoreException {
+    public NotFoundFoodStoreException() {
+        System.out.println("음식에 맞는 음식 가게를 찾을 수 없습니다.");
+    }
+}
