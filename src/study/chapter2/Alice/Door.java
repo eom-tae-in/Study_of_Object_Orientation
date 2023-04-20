518c5559dd09b5859f85975c202a1444c3caeb34
eom-tae-in <eti0728@daum.net>
2023-04-21 01:58:42 +0900

    [refactor] Alice가 통과할 문에 대한 클래스 생성

--- src/study/chapter2/Alice/Door.java
+++ src/study/chapter2/Alice/Door.java
@@ -1,2 +1,13 @@
-package src.study.chapter2.Alice;public class Door {
+package src.study.chapter2.Alice;
+
+public class Door {
+
+    private int height;
+
+    public Door() {
+        this.height = 40;
+    }
+    public boolean canPassDoor(int aliceHeight) {
+        return this.height <= aliceHeight;
+    }
 }
