d93accdd23a498e3ab5852d6ed02d2ff7d784f5f
eom-tae-in <eti0728@daum.net>
2023-04-20 21:52:05 +0900

    [refactor] checkEnoughFood 추상 메서드 반환 타입 변경

--- src/study/chapter2/Alice/Store.java
+++ src/study/chapter2/Alice/Store.java
@@ -3,6 +3,6 @@ package src.study.chapter2.Alice;
 public interface Store {
 
     void reduceWeight(int weight);
-    boolean checkEnoughFood(int quantity);
+    void checkEnoughFood(int quantity);
     boolean checkFoodInStockOrNot();
 }
