ed98e2bb908305ad4dd9765f98bb9efe21d075b7
eom-tae-in <eti0728@daum.net>
2023-04-20 10:31:46 +0900

    [refactor] MenuNotFoundException 클래스에 예외가 발생하면 출력할 출력문 추가

--- src/study/chapter1/cafe/exception/MenuNotFoundException.java
+++ src/study/chapter1/cafe/exception/MenuNotFoundException.java
@@ -1,4 +1,8 @@
 package src.study.chapter1.cafe.exception;
 
 public class MenuNotFoundException extends IllegalArgumentException{
+
+    public MenuNotFoundException() {
+        System.out.println("손님께서 주문하시려는 메뉴가 저희 카페에 없습니다.");
+    }
 }
