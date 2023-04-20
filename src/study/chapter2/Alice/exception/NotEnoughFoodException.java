58ec2e0492f60e5538ef3f7df1b1d234693b3bc5
eom-tae-in <eti0728@daum.net>
2023-04-20 21:53:12 +0900

    [feat] 음식의 양이 부족할 때 발생시킬 예외 클래스 생성

--- src/study/chapter2/Alice/exception/NotEnoughFoodException.java
+++ src/study/chapter2/Alice/exception/NotEnoughFoodException.java
@@ -1,2 +1,7 @@
-package src.study.chapter2.Alice.exception;public class NotEnoughFoodException {
+package src.study.chapter2.Alice.exception;
+
+public class NotEnoughFoodException extends IllegalArgumentException{
+    public NotEnoughFoodException() {
+        System.out.println("음식이 충분하지 않습니다. 더 적은 양으로 다시 입력해주세요.");
+    }
 }
