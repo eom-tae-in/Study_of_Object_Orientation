af55662ecae642e8e8dcfe122ff281fb456a0399
eom-tae-in <eti0728@daum.net>
2023-04-21 04:07:58 +0900

    [refactor] static 키워드 제거

--- src/study/chapter2/Alice/Output.java
+++ src/study/chapter2/Alice/Output.java
@@ -2,24 +2,24 @@ package src.study.chapter2.Alice;
 
 class Output {
 
-    static void printAskHeight() {
+    void printAskHeight() {
         System.out.println("Alice의 키를 입력해주세요: ");
     }
 
-    static void printSelectFoodQuestion() {
+    void printSelectFoodQuestion() {
         System.out.println("Alice에게 어떤 음식을 먹일건가요?");
         System.out.println("고기를 먹일거면 meat, 채소를 먹일거면 vegetable을 입력해주세요");
     }
 
-    static void printQuantityQuestion() {
+    void printQuantityQuestion() {
         System.out.println("얼마나 먹일건가요?");
     }
 
-    static void printFailEnterGarden() {
+    void printFailEnterGarden() {
         System.out.println("Alice는 정원에 들어갈 수 없습니다.");
     }
 
-    static void printSuccessEnterGarden() {
+    void printSuccessEnterGarden() {
         System.out.println("Alice가 정원에 들어갔습니다.");
     }
 }
