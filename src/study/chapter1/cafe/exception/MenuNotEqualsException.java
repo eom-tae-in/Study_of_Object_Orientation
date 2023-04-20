7a063b7b871f99985511cae508d9f76acaeb63ad
eom-tae-in <eti0728@daum.net>
2023-04-20 10:30:54 +0900

    [refactor] MenuNotEqualsException 클래스에 예외가 밣생하면 출력할 출력문 추가

--- src/study/chapter1/cafe/exception/MenuNotEqualsException.java
+++ src/study/chapter1/cafe/exception/MenuNotEqualsException.java
@@ -1,4 +1,8 @@
 package src.study.chapter1.cafe.exception;
 
 public class MenuNotEqualsException extends IllegalArgumentException{
+
+    public MenuNotEqualsException() {
+        System.out.println("손님이 주문한 메뉴와 바리스타가 만든 메뉴가 다릅니다.");
+    }
 }
