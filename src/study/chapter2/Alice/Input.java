bbf31c2d2fba5a7111b5fe1612c709558112bab8
eom-tae-in <eti0728@daum.net>
2023-04-20 23:13:37 +0900

    [refactor] alice에게 먹일 양을 입력 받는 메서드에서 예외 발생시 다시 입력을 받도록 로직 수정

--- src/study/chapter2/Alice/Input.java
+++ src/study/chapter2/Alice/Input.java
@@ -1,17 +1,36 @@
 package src.study.chapter2.Alice;
 
+import src.study.chapter2.Alice.exception.NotEnoughFoodException;
+import src.study.chapter2.Alice.exception.NotFoundFoodException;
+
 import java.util.Scanner;
 
 public class Input {
 
-    static Scanner sc = new Scanner(System.in);
-    public static int height() {
+    private Store store;
+
+    Scanner sc = new Scanner(System.in);
+    public int height() {
         return sc.nextInt();
     }
-    public static String food() {
-        return sc.next();
+    public String food() {
+        try {
+            String food = sc.next();
+            StoreAdapter.checkStoreExist(food);
+            return food;
+        } catch (NotFoundFoodException e) {
+            System.out.println(e.getMessage());
+            return food();
+        }
     }
-    public static int quantity() {
-        return sc.nextInt();
+    public int quantity(Store store) {
+        try {
+            int quantity = sc.nextInt();
+            store.checkEnoughFood(quantity);
+            return quantity;
+        } catch (NotEnoughFoodException e) {
+            System.out.println(e.getMessage());
+            return quantity(store);
+        }
     }
 }
