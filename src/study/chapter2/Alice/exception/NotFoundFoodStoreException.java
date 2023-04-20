75615baaa9b8291cab486a6be3914f15af2314f3
eom-tae-in <eti0728@daum.net>
2023-04-21 00:59:14 +0900

    [refactor] IllegalArgumentException을 상속받도록 수정

--- src/study/chapter2/Alice/exception/NotFoundFoodStoreException.java
+++ src/study/chapter2/Alice/exception/NotFoundFoodStoreException.java
@@ -1,6 +1,6 @@
 package src.study.chapter2.Alice.exception;
 
-public class NotFoundFoodStoreException {
+public class NotFoundFoodStoreException extends IllegalArgumentException{
     public NotFoundFoodStoreException() {
         System.out.println("음식에 맞는 음식 가게를 찾을 수 없습니다.");
     }
