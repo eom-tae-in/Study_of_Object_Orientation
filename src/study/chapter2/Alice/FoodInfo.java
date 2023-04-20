e7ebe03935c799d637d00ad7bb8e7b311b984327
eom-tae-in <eti0728@daum.net>
2023-04-21 00:27:01 +0900

    [feat] alice가 음식을 먹었을 때 이 음식이 키를 크게하는지 작게 하는지를 결정하는 enum 클래스 생성

---
+++ src/study/chapter2/Alice/FoodInfo.java
@@ -0,0 +1,13 @@
+package src.study.chapter2.Alice;
+
+public enum FoodInformation {
+    ;
+
+    private static final String PLUS = "plus";
+    private static final String MINUS = "minus";
+
+    VEGETABLE(PLUS), MEAT(MINUS);
+
+    FoodInformation(String ) {
+    }
+}
