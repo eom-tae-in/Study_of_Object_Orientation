c705810704d3b0c147edb246644e5be0ea4257ae
eom-tae-in <eti0728@daum.net>
2023-04-25 02:03:08 +0900

    [refactor] 입력으로 들어온 음식이 없을 때 발생시킬 예외 출력문구 추가

--- src/study/chapter2/Alice/exception/NotFoundFoodException.java
+++ src/study/chapter2/Alice/exception/NotFoundFoodException.java
@@ -3,5 +3,6 @@ package src.study.chapter2.Alice.exception;
 public class NotFoundFoodException extends IllegalArgumentException {
     public NotFoundFoodException() {
         System.out.println("해당 음식을 찾을 수 없습니다.");
+        System.out.println("다른 음식으로 다시 입력해주세요.");
     }
 }
