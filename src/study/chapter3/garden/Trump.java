985581273e8ba48732df97727ec3d069c84070f2
eom-tae-in <eti0728@daum.net>
2023-04-26 03:48:22 +0900

    [refactor] walking() 메서드의 출력문구 수정

--- src/study/chapter3/garden/Trump.java
+++ src/study/chapter3/garden/Trump.java
@@ -3,7 +3,6 @@ package src.study.chapter3.garden;
 public abstract class Trump implements Member {
     @Override
     public void walking() {
-        System.out.println("트럼프" +
-                "들을 뒤뚱뒤뚱 걷습니다.");
+        System.out.println("트럼프들을 뒤뚱뒤뚱 걷습니다.");
     }
 }
